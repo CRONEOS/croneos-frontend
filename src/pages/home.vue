c1fe0992f45b65c4e08c7787485d317c0ac44f36
unknown <kasperkasperfish@gmail.com>
2019-11-18 12:04:06 +0100

    sync

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,73 +1,132 @@
 <template>
   <q-page class=" full-height">
-
     <!-- <div class="diagonal"></div> -->
     <div class="header_box relative-position">
       <q-parallax src="./statics/images/bg.jpg" :height="450">
-        
-            <div class="text-center text-primary" style="">
-                <div style="background:#272822F2" class=" text-h3  text-uppercase text-weight-light">Mine Scheduled Actions</div>
-                <div class="text-caption bg-primary text-secondary text-weight-bold">* A replacement for deferred transactions on EOS *</div>
-                <q-btn to="mine" label="start mining" color="primary" size="lg" class="q-mt-xl" />
-            </div> 
-        
+        <div class="text-center text-primary" style="">
+          <div
+            style="background:#272822F2"
+            class=" text-h3  text-uppercase text-weight-light"
+          >
+            Mine Scheduled Actions
+          </div>
+          <div class="text-caption bg-primary text-secondary text-weight-bold">
+            * A replacement for deferred transactions on EOS *
+          </div>
+          <q-btn
+            to="mine"
+            label="start mining"
+            color="primary"
+            size="lg"
+            class="q-mt-xl"
+          />
+        </div>
       </q-parallax>
-      <div class="absolute-bottom-right text-caption q-pa-xs q-ma-xs bg-secondary cursor-pointer" style="opacity:0.9">
-        <span>#cronjobs </span><span><b>{{getNumberOfCronjobs}}</b></span>
+      <div
+        class="absolute-bottom-right text-caption q-pa-xs q-ma-xs bg-secondary cursor-pointer"
+        style="opacity:0.9"
+      >
+        <span>#cronjobs </span
+        ><span
+          ><b>{{ getNumberOfCronjobs }}</b></span
+        >
         <q-tooltip content-class="bg-secondary" :delay="500">
-          There are {{getNumberOfCronjobs}} jobs scheduled.
+          There are {{ getNumberOfCronjobs }} jobs scheduled.
         </q-tooltip>
       </div>
-    </div> 
-
-
-
-  <div  class="center-page-content" >
-
-    <div class="q-pa-sm text-white">
-      
-      <h5 class="text-center">Background</h5>
-
-      <p>Delayed transactions will be deprecated (EOSIO version xx) due to multiple issues it generates (list some of the issues). We propose CRONEOS as an alternative solution for enabling dapps to schedule future work. Smart contract developers can submit their scheduled tasks to an on chain pool. Miners can then execute these jobs. To align incentives we introduce an optional gas fee (multiple tokens allowed). The executer of the scheduled action will receive a (to be determined) portion of the fee as reward (see more about gas-reward model and relationship with CRON token). </p>
-
-      <p>The project is currently in development (live on jungle). You want to help? Contact via Github.</p>
-      <!-- <p>Croneos is looking for contributers (design infographics, website, community management, ...).</p> -->
-      
     </div>
 
-    <div class="row q-col-gutter-lg text-white">
-      <div class="col-xs-12 col-sm-6 ">
+    <div class="bg-secondary q-pb-xl">
+      <div class="q-pa-sm text-white center-page-content">
+        <h5 class="text-center text-weight-light">Background</h5>
+
+        <p>
+          Delayed transactions will be deprecated (EOSIO version xx) due to
+          multiple issues it generates (list some of the issues). We propose
+          CRONEOS as an alternative solution for enabling dapps to schedule
+          future work. Smart contract developers can submit their scheduled
+          tasks to an on chain pool. Miners can then execute these jobs. To
+          align incentives we introduce an optional gas fee (multiple tokens
+          allowed). The executer of the scheduled action will receive a (to be
+          determined) portion of the fee as reward (see more about gas-reward
+          model and relationship with CRON token).
+        </p>
+
+        <p>
+          The project is currently in development (live on jungle). You want to
+          help? Contact via Github.
+        </p>
+        <!-- <p>Croneos is looking for contributers (design infographics, website, community management, ...).</p> -->
+      </div>
+    </div>
+
+    <div class="bg-primary-light q-pb-xl">
+      <div class="q-pa-sm text-black center-page-content">
+        <h5 class="text-center text-weight-light">Decentralized & Trustless</h5>
+        <div>
+          The execution of the scheduled jobs don't depend on a single entity.
+          Anyone can execute jobs from the pool. When using croneos for your
+          scheduled tasks you don't need to rely on scripts or non native
+          services that can fail. Croneos is a base layer solution for scheduled
+          jobs and lives entirly on the main chain.
+        </div>
+      </div>
+    </div>
+
+    <div class="bg-secondary q-pb-xl">
+      <div class="q-pa-sm text-black center-page-content">
+        <div class="row q-col-gutter-lg text-white">
+          <div class="col-xs-12 col-sm-6 ">
+            <div class="q-pa-sm">
+              <h5 class="text-center text-weight-light">Developers</h5>
+              <p>
+                Smart contract devs can integrate croneos by including a single
+                header file (c++) to make it easy to interact with the croneos
+                smart contract. The api to schedule jobs is very similar to
+                normal inline transactions. You'll need to configure the execution time and add an optional gas fee.
+              </p>
+              <router-link to="/docs/getting-started" tag="a" class="text-link">
+                Get started
+              </router-link>
+            </div>
+          </div>
+          <div class="col-xs-12 col-sm-6 ">
+            <div class="q-pa-sm">
+              <h5 class="text-center text-weight-light">Miners</h5>
+              <p>
+                Monetize your excess CPU by mining scheduled jobs. As a miner
+                you earn rewards for each successful execution. We encourage automated mining bots.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="bg-primary-light q-pb-xl">
+      <div class="q-pa-sm text-black center-page-content">
         <div class="q-pa-sm">
-          <h5 class="text-center">Developers</h5>
-          <p>Smart contract devs can integrate croneos by including a single header file (c++). only pay optional  fee if the cron job is executed successful else refund (expired, cancelled). Higher fees will increase your odds of on time execution.</p>
-          <router-link to="/docs/getting-started" tag="a" class="text-link">
-            Get started
+          <h5 class="text-center text-weight-light">
+            Aligning Incentives with Gas
+          </h5>
+          <p>
+            Although paying gas is optional we believe it will increase the
+            probability of on time execution. The executer of your scheduled
+            action will be rewarded with % (to be determined) of the gas and
+            additional CRON tokens. Currently the CRONEOS smart contract only
+            accepts the following gas tokens:
+          </p>
+          <gas-tokens />
+
+          <router-link to="/gas-tokens" tag="a" class="text-link">
+            Apply for listing your token
           </router-link>
-          
         </div>
       </div>
-      <div class="col-xs-12 col-sm-6 ">
-        <div class="q-pa-sm">
-          <h5 class="text-center">Miners</h5>
-          <p>Monetize your excess CPU by mining scheduled jobs. As a miner you earn rewards for each successful execution. See allowed fee tokens...</p>
-        </div>
-      </div>
-
-    </div>
-  
-    <div class="q-pa-sm text-white">
-      <h5 class="text-center">Aligning Incentives with Gas</h5>
-      <p>Although paying gas is optional we believe it will increase the probability of on time execution. The executer of your scheduled action will be rewarded with 50% (to be determined) of the gas and additional CRON tokens.  Currently the CRONEOS smart contract only accepts the following gas tokens:</p>
-      <gas-tokens />
-
-      <router-link to="/gas-tokens" tag="a" class="text-link">
-        Apply for listing your token
-      </router-link>
     </div>
 
-
     <div class="q-pa-sm text-white">
-      <h5 class="text-center">CRON Token</h5>
+      <h5 class="text-center text-weight-light">CRON Token</h5>
     </div>
     <p class="text-center">More info about the CRON token soon...</p>
     <!-- <div class="row q-col-gutter-lg text-white q-pa-sm">
@@ -94,20 +153,15 @@
 
     </div> -->
 
-
     <div class="q-pa-sm text-white">
-      <h5 class="text-center">...add info graphics...</h5>
-      <div class="text-center" >I'm not tallented enough to make these. If you're a designer and want to help, contact via github.</div>
+      <h5 class="text-center text-weight-light">...Design and infographics...</h5>
+      <div class="text-center">
+        I'm not tallented enough to design nice websites let alone infographics. If you're a designer and want to
+        help, please reach out via our telegram.
+      </div>
     </div>
 
-
-
-
-  </div><!-- end center page -->
-
-<div style="height:200px"></div>
-
-
+    <div style="height:200px"></div>
 
     <!-- <div class="row justify-center overflow-hidden" style="">
         <span>Schedule contract actions for execution in the future. Receive rewards by executing scheduled tasks.</span>
@@ -128,19 +182,14 @@
         <li>miners execute the scheduled jobs and receive rewards (CRON token and %  fee)</li>
       </ul>
     </div> -->
-
-
-
   </q-page>
 </template>
 
-<style>
-</style>
+<style></style>
 
 <script>
 import { mapGetters } from "vuex";
-import gasTokens from 'components/gas-tokens'
-
+import gasTokens from "components/gas-tokens";
 
 export default {
   name: "PageIndex",
@@ -180,7 +229,7 @@ export default {
   border-radius: 8px;
   box-sizing: border-box;
 }
-.header_box{
-  border-bottom: 5px solid  var(--q-color-primary)
+.header_box {
+  border-bottom: 5px solid var(--q-color-primary);
 }
 </style>
